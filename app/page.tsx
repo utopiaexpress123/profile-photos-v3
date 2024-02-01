3e8c088acfa61502a1e2c82987c8738d5c9de56f
rick-sys <ricksanchezy2x@gmail.com>
2024-02-01 23:16:41 +0100

    botpress

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import hero from "/public/hero.png";
 import { Button } from "@/components/ui/button";
 import ExplainerSection from "@/components/ExplainerSection";
 import PricingSection from "@/components/PricingSection";
+import ChatBot from '/components/ChatBot';
 export const dynamic = "force-dynamic";
 
 export default async function Index() {
@@ -58,6 +59,7 @@ export default async function Index() {
       </div>
       <ExplainerSection />
       <PricingSection />
+      <ChatBot />
     </div>
   );
 }
