cd04ad630fb582c1cc212ea484711afda6d6dcaa
rick-sys <ricksanchezy2x@gmail.com>
2024-03-01 14:51:10 +0100

    aa

--- components/component/faq-accordion.tsx
+++ components/component/faq-accordion.tsx
@@ -3,6 +3,9 @@
  * @see https://v0.dev/t/WJcVn7VLiFa
  */
 import { CollapsibleTrigger, CollapsibleContent, Collapsible } from "@/components/ui/collapsible"
+export interface ChevronDownIcon {
+  className?: string;
+}
 
 export function FaqAccordion() {
   return (
@@ -81,21 +84,4 @@ export function FaqAccordion() {
 }
 
 
-function ChevronDownIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m6 9 6 6 6-6" />
-    </svg>
-  )
-}
+
